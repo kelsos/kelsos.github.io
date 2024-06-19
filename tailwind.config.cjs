52dfc9b8b013f8da971078fd0192d1581183a10e
Konstantinos Paparas <kelsos86@gmail.com>
2024-06-19 11:37:26 +0200

    chore: update dependencies

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+export default {
+  mode: 'jit',
+  darkMode: 'class',
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+  },
+};
